f92637a719a0965ea46a3634b4a3b6f901424817
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-09-23 20:02:43 +0800

    triangle

---
+++ Fourth/triangle.java
@@ -0,0 +1,20 @@
+package JAVA;
+
+public class triangle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<9;i++)
+		{
+			for(int j=0;j<i+1;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
