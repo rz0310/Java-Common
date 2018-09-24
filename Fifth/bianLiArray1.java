5317233f5979c7e3fe86e47610f24d790607aabc
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-09-24 09:50:19 +0800

    Array

---
+++ Fifth/bianLiArray1.java
@@ -0,0 +1,19 @@
+package HomeWork;
+
+public class bianLiArray1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//数组遍历(依次输出数组中的每一个元素)
+		//定义一维数组
+		int arr[]= new int[]{1,2,3,4,5,6,7,8,9};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+
+}
