7c3f8cce8d6419d4da9fdd88f355ccdaaede01b7
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-09-27 20:44:02 +0800

    Work

---
+++ Eight/Day5HomeWork4Student.java
@@ -0,0 +1,11 @@
+package HomeWork;
+/*.定义一个学生类Student，包含三个属性姓名、年龄、性别，创建三个学生对象存入ArrayList集合中。
+  A：遍历集合遍历输出。
+  B：求出年龄最大的学生
+*/
+public class Day5HomeWork4Student {
+	String name;
+	int age;
+	String sex;
+}
+
