443160ce8662fbed148a76f9a6b15521a5677866
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-09-27 20:44:30 +0800

    work

---
+++ Eight/Day5HomeWork5Phone.java
@@ -0,0 +1,10 @@
+package HomeWork;
+/*定义一个手机类Phone，包含三个属性品牌(brand)、价格(price)、颜色(color)。
+  创建四个手机("小米"、"华为"、"乐视"、"奇酷")对象存入ArrayList集合中，并遍历输出。*/
+public class Day5HomeWork5Phone {
+
+	String brand;
+	double price;
+	String color;
+}
+
