8ea2b10f23434c5ef1894f4a649b100a148b6f7d
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-10-08 19:16:44 +0800

    Student2

---
+++ Eighteen/Student2.java
@@ -0,0 +1,22 @@
+package Day5;
+
+public class Student2 {
+	String name;
+	int age;
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setAge(int age)
+	{
+		this.age=age;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+}
