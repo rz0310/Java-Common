c3c29dea3daab2376d69051576ea5be302bb28b4
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-10-11 21:26:55 +0800

    JavaEE

---
+++ Ninteen/JavaEE.java
@@ -0,0 +1,11 @@
+package Employee;
+
+public class JavaEE extends Developer{
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		System.out.println("编号为:"+super.getId()+"  姓名为:"+super.getName()+"的JavaEE开发正在进行淘宝软件开发");
+	}
+	
+}
