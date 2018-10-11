497fe3a7a4f11df05ec61f50ba3bd2890b29f548
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-10-11 21:26:34 +0800

    Android

---
+++ Ninteen/Android.java
@@ -0,0 +1,10 @@
+package Employee;
+
+public class Android extends Developer{
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		System.out.println("编号为:"+getId()+"  姓名为:"+getName()+"的Android开发工程师正在进行手机淘宝开发");
+	}
+}
