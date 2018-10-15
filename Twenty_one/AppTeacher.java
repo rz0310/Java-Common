5b72782a604413ac1f66a215e55b7da3ab88e38f
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-10-15 20:20:30 +0800

    AppTeacher
    AppTeacher

---
+++ Twenty_one/AppTeacher.java
@@ -0,0 +1,12 @@
+package HomeWork2;
+
+public class AppTeacher extends Teacher {
+
+	@Override
+	//讲课的方法
+	public void teach() {
+		// TODO Auto-generated method stub
+		System.out.println("App班老师正在讲Android");
+	}
+
+}
