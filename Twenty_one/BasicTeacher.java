b42030b5fa03251d823492b2b4078cf803e76048
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-10-15 20:20:42 +0800

    BasicTeacher
    BasicTeacher

---
+++ Twenty_one/BasicTeacher.java
@@ -0,0 +1,14 @@
+package HomeWork2;
+/*
+ * 定义基础班老师类
+ */
+public class BasicTeacher extends Teacher{
+
+	@Override
+	//讲课的方法
+	public void teach() {
+		// TODO Auto-generated method stub
+		System.out.println("基础班老师正在讲JavaSE");
+	}
+
+}
