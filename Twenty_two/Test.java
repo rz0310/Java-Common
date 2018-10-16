e400089f11c9eca85cfa31bbc4171e70a0c94aba
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-10-16 22:46:37 +0800

    Test
    Test

---
+++ Twenty_two/Test.java
@@ -0,0 +1,14 @@
+package lianXi2;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		newPhone p = new newPhone();
+		p.showNum();
+	}
+
+}
